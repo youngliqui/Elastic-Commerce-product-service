49edd8bb3119fe952cd2bbe1e2ff4f52bc44f402
youngliqui <super.pav-79@yandex.by>
2025-10-11 16:23:07 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+PRODUCT-SERVICE for Elastic-Commerce
---
+++ src/main/kotlin/by/youngliqui/productservice/ElasticCommerceProductServiceApplication.kt
@@ -0,0 +1,11 @@
+package by.youngliqui.productservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElasticCommerceProductServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ElasticCommerceProductServiceApplication>(*args)
+}
